c8dca7265797ef938c32919152857896b96e4662
Pierre Salaun <pierresalaun10@hotmail.com>
2018-10-13 19:42:34 +0200

    create tests for challenge 1

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -1,8 +1,6 @@
 // Exercice 1
 // Find the last element of a list.
 
-function lastElement(list) {
+exports.lastElement = function(list) {
   return list[list.length - 1];
 }
-
-console.log(lastElement([1,2,3,4,5]))
--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+var expect    = require("chai").expect;
+var challenges = require("../javascript/challenges");
+
+describe("Find the last element of a list", function() {
+  it("returns the last element of a list of digits", function() {
+    const digits = challenges.lastElement([1, 2, 3, 4, 5]);
+    expect(digits).to.equal(5);
+  });
+  it("returns the last element of a list of letters", function() {
+    const letters = challenges.lastElement(["a", "b", "c", "d", "abcdef"]);
+    expect(letters).to.equal("abcdef");
   });
 });
