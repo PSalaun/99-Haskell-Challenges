ec0a265ec8335e0e420622ce2c988c5f13675aa0
Pierre Salaun <pierresalaun10@hotmail.com>
2018-10-13 20:07:03 +0200

    add eslint

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -1,12 +1,12 @@
 // Exercice 1
 // Find the last element of a list.
 
-exports.lastElement = function(list) {
+exports.lastElement = function (list) {
   return list[list.length - 1];
-}
+};
 
 // Exercice 2 - Find the last but one element of a list
 
-exports.lastButOneElement = function(list) {
+exports.lastButOneElement = function (list) {
   return list[list.length - 2];
-}
+};
