c8ed538f0c96b40c517529587d7adfcdd3b55f90
Pierre Salaun <pierresalaun10@hotmail.com>
2018-10-13 19:59:16 +0200

    exercise 2 solved

--- javascript/challenges.js
+++ javascript/challenges.js
@@ -4,3 +4,9 @@
 exports.lastElement = function(list) {
   return list[list.length - 1];
 }
+
+// Exercice 2 - Find the last but one element of a list
+
+exports.lastButOneElement = function(list) {
+  return list[list.length - 2];
+}
